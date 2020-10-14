3fef4dbdb63c70dbd65d00dec5b7925fa4a97fe5
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-10-13 22:53:07 -0300

    Allow other apps to access backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './database/connection';
@@ -9,7 +10,8 @@ import errorHandler from './errors/handlers';
 
 const app = express()
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
