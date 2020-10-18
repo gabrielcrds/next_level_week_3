87670f0492494e148503513a03e3a684ac2e5a84
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-10-18 16:47:01 -0300

    Adds new pages and header components

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -4,12 +4,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/header';
+
 const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes(){
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown: false}}>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
                 <Screen
                     name="OrphanagesMap"
                     component={OrphanagesMap}
@@ -17,6 +22,26 @@ export default function Routes(){
                 <Screen
                     name="OrphanageDetails"
                     component={OrphanageDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Orfanato"/>
+                    }}
+                />
+                <Screen
+                    name="SelectMapPosition"
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione no mapa" />
+                    }}
+                />
+                <Screen
+                    name="OrphanageData"
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os dados" />
+                    }}
                 />
             </Navigator>
         </NavigationContainer>
